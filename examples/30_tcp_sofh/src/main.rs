a44e58037822497ad3af15d079d9b5d72cb2dd72
pan93412 <pan93412@gmail.com>
2022-05-23 03:08:17 +0000

    fix(ex/30_tcp_sofh): use our new Iterator impl

--- examples/30_tcp_sofh/src/main.rs
+++ examples/30_tcp_sofh/src/main.rs
@@ -6,9 +6,9 @@ fn main() -> io::Result<()> {
     let listener = net::TcpListener::bind("127.0.0.1:8080")?;
     let reader = listener.accept()?.0;
     let decoder = SeqDecoder::default();
-    let mut frames = decoder.read_frames(reader);
-    while let Ok(frame) = frames.next() {
-        if let Some(frame) = frame {
+    let frames = decoder.read_frames(reader);
+    for frame in frames {
+        if let Ok(frame) = frame {
             let payload_clone = &frame.payload().to_vec()[..];
             let payload_utf8 = String::from_utf8_lossy(payload_clone);
             println!("Received message '{}'", payload_utf8);
